5c1829179e972fc22423457fc7194b8d4a0f4663
nishit <nishitmohanan@gmail.com>
2020-04-16 01:46:10 +0530

    removed extra whitespace

--- stream_sync/src/index.js
+++ stream_sync/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 import Router from "./components/Router";
 import * as serviceWorker from "./serviceWorker";
-
 ReactDOM.render(
   <React.StrictMode>
     <Router />
