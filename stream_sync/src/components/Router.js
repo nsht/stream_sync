69ea10ecb7a467fb2b3e76dd5b4a30e9e5987a98
nishit <nishitmohanan@gmail.com>
2020-04-16 01:46:42 +0530

    Added host route

--- stream_sync/src/components/Router.js
+++ stream_sync/src/components/Router.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
+import Host from "./Host";
+
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/host" component={Host} />
       {/* <Route exact path="/article/:article" component={ArticlePage} /> */}
     </Switch>
   </BrowserRouter>
