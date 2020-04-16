c7c8008ccf42571fe3a7b661f0c5e361e9f12348
nishit <nishitmohanan@gmail.com>
2020-04-17 01:54:00 +0530

    Added function to parse video id from url

--- stream_sync/src/components/Host.js
+++ stream_sync/src/components/Host.js
@@ -5,63 +5,82 @@ import Navbar from "./Navbar";
 class Host extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
-    if (this.roomNameRef.current.value === "") {
-      return;
+    var video_id = this.parseIdFromURL(e.target.youtubeLink.value);
+    console.log(video_id);
+    // if (this.youtubeLinkRef.current.value === "") {
+    //   return;
+    // }
+    // console.log(this.youtubeLinkRef.current.value);
+  };
+  parseIdFromURL = url => {
+    console.log(url);
+    var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    console.log(match);
+    if (match && match[2].length == 11) {
+      return match[2];
+    } else {
+      return false;
     }
-    console.log(this.chatInputRef.current.value);
   };
   render() {
     return (
       <>
         <Navbar></Navbar>
 
-        <section class="section">
+        <section className="section">
           <div className="container">
-            <div class="columns is-centered">
-              <div class="column is-half">
-              <div class="card">
-  <div class="card-content">
-                <form onSubmit={this.handleSubmit} className="room_name">
-                  <div class="field">
-                    <label class="label">RoomName</label>
-                    <div class="control">
-                      <input
-                        class="input"
-                        ref={this.roomNameRef}
-                        placeholder="Type the name of your room"
-                        type="text"
-                      ></input>
-                    </div>
-                  </div>
+            <div className="columns is-centered">
+              <div className="column is-half">
+                <div className="card">
+                  <div className="card-content">
+                    <form onSubmit={this.handleSubmit} className="room_name">
+                      <div className="field">
+                        <label className="label">RoomName</label>
+                        <div className="control">
+                          <input
+                            className="input"
+                            ref={this.roomNameRef}
+                            placeholder="Type the name of your room"
+                            type="text"
+                            name="roomName"
+                          ></input>
+                        </div>
+                      </div>
 
-                  <div class="field">
-                    <label class="label">Username</label>
-                    <div class="control">
-                      <input
-                        class="input"
-                        ref={this.hostUserName}
-                        placeholder="Please enter your username"
-                        type="text"
-                      ></input>
-                    </div>
-                  </div>
+                      <div className="field">
+                        <label className="label">Username</label>
+                        <div className="control">
+                          <input
+                            className="input"
+                            ref={this.hostUserName}
+                            placeholder="Please enter your username"
+                            type="text"
+                            name="userName"
+                          ></input>
+                        </div>
+                      </div>
 
-                  <div class="field">
-                    <label class="label">Youtube Link</label>
-                    <div class="control">
-                      <input
-                        class="input"
-                        ref={this.hostUserName}
-                        placeholder="The Youtube link to be shared"
-                        type="text"
-                      ></input>
-                    </div>
+                      <div className="field">
+                        <label className="label">Youtube Link</label>
+                        <div className="control">
+                          <input
+                            className="input"
+                            ref={this.youtubeLinkRef}
+                            placeholder="The Youtube link to be shared"
+                            type="url"
+                            name="youtubeLink"
+                          ></input>
+                        </div>
+                      </div>
+                      <div className="buttons is-right">
+                        <button className="button is-primary is-light is-right">
+                          Party🎉{" "}
+                        </button>
+                      </div>
+                    </form>
                   </div>
-                  <div class="buttons is-right">
-                  <button class="button is-primary is-light is-right">Party🎉 </button>
-                  </div>
-                </form>
-                </div></div>
+                </div>
               </div>
             </div>
           </div>
