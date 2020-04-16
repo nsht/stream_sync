b212f1c325bdd5592342a4882be6d7b25302c97b
nishit <nishitmohanan@gmail.com>
2020-04-17 01:54:24 +0530

    Renamed class to className in navbar

--- stream_sync/src/components/Navbar.js
+++ stream_sync/src/components/Navbar.js
@@ -4,59 +4,59 @@ import "../css/App.css";
 class Navbar extends React.Component {
   render() {
     return (
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
             <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"></img>
           </a>
       
-          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
       
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item">
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item">
               Home
             </a>
       
-            <a class="navbar-item">
+            <a className="navbar-item">
               Documentation
             </a>
       
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">
                 More
               </a>
       
-              <div class="navbar-dropdown">
-                <a class="navbar-item">
+              <div className="navbar-dropdown">
+                <a className="navbar-item">
                   About
                 </a>
-                <a class="navbar-item">
+                <a className="navbar-item">
                   Jobs
                 </a>
-                <a class="navbar-item">
+                <a className="navbar-item">
                   Contact
                 </a>
-                <hr class="navbar-divider"></hr>
-                <a class="navbar-item">
+                <hr className="navbar-divider"></hr>
+                <a className="navbar-item">
                   Report an issue
                 </a>
               </div></div>
             </div>
           </div>
       
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-                <a class="button is-primary">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
                   <strong>Sign up</strong>
                 </a>
-                <a class="button is-light">
+                <a className="button is-light">
                   Log in
                 </a>
               </div>
