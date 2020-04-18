30e006daef9518de01d487372b34ec252ef2d226
nishit <nishitmohanan@gmail.com>
2020-04-19 01:09:59 +0530

    Added party route

--- stream_sync/src/components/Router.js
+++ stream_sync/src/components/Router.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Host from "./Host";
-
+import Party from "./Party";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/host" component={Host} />
-      {/* <Route exact path="/article/:article" component={ArticlePage} /> */}
+      <Route exact path="/party" component={Host} />
+      <Route exact path="/party/:host_id" component={Party} />
     </Switch>
   </BrowserRouter>
 );
