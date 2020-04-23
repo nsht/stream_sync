2d0c000da6468ec70287a4f393659f47c4962e38
nishit <nishitmohanan@gmail.com>
2020-04-24 00:38:59 +0530

    Added broadcast functions

--- stream_sync/src/utils/webRTC_utils.js
+++ stream_sync/src/utils/webRTC_utils.js
@@ -3,12 +3,13 @@ export var global_this_obj = null;
 export function createConnection(thisObj, is_host, host_id = null) {
   const Peer = window.Peer;
   var peer = new Peer({ debug: 2 });
-
+  window.peer_obj = peer;
+  window.is_host = is_host;
   if (is_host !== true) {
     var conn = peer.connect(host_id);
     handle_connection(conn);
   }
-  // var peer = new Peer("test_nishit");
+
   peer.on("open", function(id) {
     console.log("MY peer ID is " + peer.id);
     thisObj.setState({
@@ -21,32 +22,60 @@ export function createConnection(thisObj, is_host, host_id = null) {
     console.log("connection called");
     handle_connection(conn);
   });
-
-  function handle_connection(conn) {
-    window.peer_ids.push(conn.peer);
-    conn.on("data", function(data) {
-      console.log(data);
-      data_handler(data);
-    });
-
-    conn.on("close", function() {
-      conn = null;
-    });
-
-    window.connections.push(conn);
-    // broadcast_new_connection(conn.peer); TODO
-  }
-  return peer.id;
 }
 
+function handle_connection(conn) {
+  window.peer_ids.push(conn.peer);
+  conn.on("data", function(data) {
+    console.log("data received");
+    console.log(data);
+    data_handler(data);
+  });
+  console.log("Handled connection");
+  conn.on("close", function() {
+    conn = null;
+  });
+
+  window.connections.push(conn);
+  // TODO if host send youtube data here
+  // setTimeout(function() {
+  //   send_data(window.global_this_obj.state);
+  // }, 500);
+  if (window.is_host === true) {
+    broadcast_new_connection(conn.peer);
+  }
+}
 function data_handler(data) {
-  console.log("IN data handler");
   console.log(data);
   if (typeof data === "object" && data !== null) {
-    if (data.data_type == "chat") {
+    if (data.data_type === "chat") {
       chat_handler(data);
+    } else if (data.data_type === "new_connection") {
+      connect_to_peer(data.peer_id);
     }
   }
+}
+
+function send_data(data) {
+  var connections = window.connections;
+  for (var i = 0; i < connections.length; i++) {
+    connections[i].send(data);
+  }
+}
+
+function broadcast_new_connection(peer_id) {
+  var msg = { data_type: "new_connection", peer_id: peer_id };
+  for (var i = 0; i < window.connections.length; i++) {
+    if (window.connections[i].peer == peer_id) {
+      continue;
+    }
+    window.connections[i].send(msg);
+  }
+}
+
+function connect_to_peer(peer_id) {
+  var conn = window.peer_obj.connect(peer_id);
+  handle_connection(conn);
 }
 
 // Chat utils
@@ -64,11 +93,7 @@ function chat_handler(chat_data) {
 
 export function send_chat(msg, user_name, is_host) {
   var msg_json = generate_chat_structure(msg, user_name, is_host);
-  console.log(msg_json);
-  var connections = window.connections;
-  for (var i = 0; i < connections.length; i++) {
-    connections[i].send(msg_json);
-  }
+  send_data(msg_json);
   chat_handler(msg_json);
 }
 
