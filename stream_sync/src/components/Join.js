3a83ddf3873b26a8e692416a85c71c7c7209bda8
nishit <nishitmohanan@gmail.com>
2020-04-25 21:38:38 +0530

    Added Join component

---
+++ stream_sync/src/components/Join.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Redirect } from "react-router-dom";
+
+class Join extends React.Component {
+  state = {
+    RoomId: ""
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ RoomId: e.target.RoomId.value });
+  };
+  render() {
+    if (this.state.RoomId !== "") {
+      return (
+        <Redirect
+          push
+          to={{
+            pathname: "party/" + this.state.RoomId,
+            state: this.state
+          }}
+        ></Redirect>
+      );
+    }
+    return (
+      <>
+        <Navbar></Navbar>
+
+        <section className="section">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-half">
+                <div className="card">
+                  <div className="card-content">
+                    <form onSubmit={this.handleSubmit} className="room_name">
+                      <div className="field">
+                        <label className="label">RoomId</label>
+                        <div className="control">
+                          <input
+                            className="input"
+                            ref={this.roomNameRef}
+                            placeholder="Type the name of your room"
+                            type="text"
+                            name="RoomId"
+                            required
+                          ></input>
+                        </div>
+                      </div>
+                      <div className="buttons is-right">
+                        <button className={"button is-primary is-right"}>
+                          Party🎉{" "}
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+}
+
+export default Join;
--- stream_sync/src/components/Router.js
+++ stream_sync/src/components/Router.js
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Host from "./Host";
 import Party from "./Party";
-
+import Join from "./Join";
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/host" component={Host} />
+      <Route exact path="/join" component={Join} />
       <Route exact path="/party" component={Host} />
       <Route exact path="/party/:host_id" component={Party} />
     </Switch>
