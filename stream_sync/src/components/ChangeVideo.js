9b4009d9abaa79fcc3cf1b75dbe7ec17a3c91b46
nishit <nishitmohanan@gmail.com>
2020-05-02 00:14:30 +0530

    Added change video component

---
+++ stream_sync/src/components/ChangeVideo.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { change_video, parseIdFromURL } from "../utils/webRTC_utils";
+
+class ChangeVideo extends React.Component {
+  changeVideo = e => {
+    e.preventDefault();
+    let video_url = e.target.video_url.value;
+    let video_id = parseIdFromURL(video_url);
+    if (video_id) {
+      change_video(video_id, true);
+    }
+  };
+
+  render() {
+    return (
+      <div className="">
+        <hr className="changeVideo_divider"></hr>
+        <form onSubmit={this.changeVideo}>
+          <div className="field is-grouped">
+            <p className="control is-expanded">
+              <input
+                className="input"
+                type="text"
+                name="video_url"
+                placeholder="Enter new video URL"
+                required
+              />
+            </p>
+            <p className="control">
+              <button className="button is-primary is-right">
+                Change Video
+              </button>
+            </p>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ChangeVideo;
