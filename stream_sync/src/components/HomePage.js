82f9759fe88d5cec5d27281be84cab608bdf5564
nishit <nishitmohanan@gmail.com>
2020-05-05 23:34:01 +0530

    Made explainer div responsive

--- stream_sync/src/components/HomePage.js
+++ stream_sync/src/components/HomePage.js
@@ -28,29 +28,44 @@ class HomePage extends React.Component {
           </div>
         </section>
         {/* TODO refactor for responsiveness with section and hero tags */}
-        <div className="row">
-          <div className="homepage_explainer" href="/join">
-            <img
-              className="laptop_svg"
-              src={process.env.PUBLIC_URL + "/laptop.svg"}
-            ></img>
-            <img
-              className="sync_svg"
-              src={process.env.PUBLIC_URL + "/sync.svg"}
-            ></img>
-            <img
-              className="laptop_svg"
-              src={process.env.PUBLIC_URL + "/laptop.svg"}
-            ></img>
+        <section className="hero is-fullheight">
+          <div className="hero-body">
+            <div className="tile is-ancestor full_height">
+              <div className="tile is-6">
+                <div className="container homepage_left_hero">
+                  <div className="homepage_explainer">
+                    <img
+                      className="laptop_svg"
+                      src={process.env.PUBLIC_URL + "/laptop.svg"}
+                    ></img>
+                    <img
+                      className="sync_svg"
+                      src={process.env.PUBLIC_URL + "/sync.svg"}
+                    ></img>
+                    <img
+                      className="laptop_svg"
+                      src={process.env.PUBLIC_URL + "/laptop.svg"}
+                    ></img>
+                  </div>
+                </div>
+              </div>
+              <div className="tile is-6">
+                <div className="homepage_explainer">
+                  <ol>
+                    <li>Host a party using a YouTube link</li>
+                    <li>Share links with friends</li>
+                    <li>Watch in sync! Chat with friends!</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
           </div>
-
-          <div className="homepage_explainer " href="/host">
-            <ol>
-              <li>Host a party using a YouTube link</li>
-              <li>Share links with friends</li>
-              <li>Watch in sync! Chat with friends!</li>
-            </ol>
-          </div>
+        </section>
+        <div className="test_row">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
         </div>
         <div className="row">
           <a className="select_column host" href="/host">
