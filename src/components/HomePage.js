85e3f89e6a4e487a814a8d20a3cfe0bdefe0122d
jeff007ali <jagharia@gmail.com>
2020-05-23 22:21:54 +0530

    Try to cheat github algo

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -21,7 +21,7 @@ class HomePage extends React.Component {
                   className="hero_video"
                   autoPlay="true"
                   loop="true"
-                  src={process.env.PUBLIC_URL + "/intro_video.mp4"}
+                  src={process.env.PUBLIC_URL + "/introductory_video.mp4"}
                 ></video>
               </div>
             </div>
